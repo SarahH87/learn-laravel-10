5d07bcf914c756e6343384005c07c105bab70b23
SarahH <sarah.heanan@komododigital.co.uk>
2023-11-14 16:07:13 +0000

    fresh install of laravel 10. Layout views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Employee List')
+
+@section('content')
+    <p>List of employees below</p>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>@yield('title')</h1>
+
+            @yield('content')
+        </div>
+    </body>
+</html>
