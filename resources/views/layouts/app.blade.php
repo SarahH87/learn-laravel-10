d82dbb86578c2ddad46142cc389ed67f2ea1c8e2
SarahH <sarah.heanan@komododigital.co.uk>
2023-11-27 21:53:10 +0000

    vue attribute binding example

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,37 @@
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>@yield('title')</title>
+        <script src="http://unpkg.com/vue@3"></script>
     </head>
 
     <body>
         <div class="container">
             <h1>@yield('title')</h1>
+
+            <div id="app">
+
+                <p>
+                    <input type="text" v-model="greeting">
+                </p>
+
+                @{{ greeting }} (@{{ greeting.length }})
+            </div>
+
+            <script>
+                Vue.createApp({
+                    data() {
+                        return {
+                            greeting: 'Hello World!'
+                        };
+                    },
+
+                    mounted() {
+                        setTimeout(() => {
+                            this.greeting = 'Changed';
+                        }, 3000);
+                    }
+                }).mount('#app');
+            </script>
 
             @yield('content')
         </div>
