8907fd32932f305d31e0a0251952fcfdd9c4fb15
SarahH <sarah.heanan@komododigital.co.uk>
2023-11-27 22:18:23 +0000

    add markup and basic styles to conditionals exercise

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <script src="http://unpkg.com/vue@3"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
 
     <body>
--- resources/views/lists-conditionals.blade.php
+++ resources/views/lists-conditionals.blade.php
@@ -4,8 +4,21 @@
 
 @section('content')
 
-    <div id="app">
-        <h1>Vue Lists, Conditionals and Computed Properties</h1>
+    <div id="app" class="h-full grid place-items-center">
+
+        <section>
+            <h1 class="text-2xl font-bold">Vue Lists, Conditionals and Computed Properties</h1>
+            <h2 class="text-xl font-bold mt-8 mb-2">Assignments</h2>
+
+            <ul>
+                <li>Finish shopping <input type="checkbox"></li>
+                <li>Learn Vue <input type="checkbox"></li>
+                <li>Laravel basics recap <input type="checkbox"></li>
+                <li>Read Laravel documentation <input type="checkbox"></li>
+                <li>Learn Docker <input type="checkbox"></li>
+                <li>Have a shower <input type="checkbox"></li>
+            </ul>
+        </section>
     </div>
 
     <script>
