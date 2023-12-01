9d8a188ff55cbf8ac895da548edcacae5618a769
SarahH <sarah.heanan@komododigital.co.uk>
2023-12-01 12:39:20 +0000

    Create assignments component
    Child component to add to page

---
+++ resources/js/Components/Assignments.vue
@@ -0,0 +1,49 @@
+<template>
+    <section v-show="inProgressAssignments.length">
+        <h2 class="text-xl font-bold mt-8 mb-2">In Progress</h2>
+
+        <ul>
+            <li v-for="assignment in inProgressAssignments"
+                :key="assignment.id">
+                <label>
+                    {{ assignment.name }}
+
+                    <input type="checkbox" v-model="assignment.complete">
+                </label>
+            </li>
+        </ul>
+    </section>
+
+    <section v-show="completedAssignments.length" class="mt-8">
+        <h2 class="text-xl font-bold">Completed</h2>
+
+        <ul>
+            <li v-for="assignment in completedAssignments"
+                :key="assignment.id">
+                <label>
+                    {{ assignment.name }}
+
+                    <input type="checkbox" v-model="assignment.complete">
+                </label>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    assignments: Object,
+})
+
+const inProgressAssignments = computed(() => {
+    return props.assignments.filter(assignment => !assignment.complete);
+})
+
+const completedAssignments = computed(() => {
+    return props.assignments.filter(assignment => assignment.complete);
+})
+
+</script>
+
--- resources/js/Pages/Conditionals.vue
+++ resources/js/Pages/Conditionals.vue
@@ -3,53 +3,18 @@
         <h1 class="text-2xl font-bold mb-2">Conditionals</h1>
         <p>Conditionals exercise below</p>
 
-        <section v-show="inProgressAssignments.length">
-            <h2 class="text-xl font-bold mt-8 mb-2">In Progress</h2>
+        <Assignments :assignments="assignments"></Assignments>
 
-            <ul>
-                <li v-for="assignment in inProgressAssignments"
-                    :key="assignment.id">
-                    <label>
-                        {{ assignment.name }}
-
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                </li>
-            </ul>
-        </section>
-
-        <section v-show="completedAssignments.length" class="mt-8">
-            <h2 class="text-xl font-bold">Completed</h2>
-
-            <ul>
-                <li v-for="assignment in completedAssignments"
-                    :key="assignment.id">
-                    <label>
-                        {{ assignment.name }}
-
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                </li>
-            </ul>
-        </section>
     </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
-import { computed } from 'vue'
+import Assignments from '@/Components/Assignments.vue';
 
-const props = defineProps({
+defineProps({
     currentPage: String,
     assignments: Object,
-})
-
-const inProgressAssignments = computed(() => {
-    return props.assignments.filter(assignment => !assignment.complete);
-})
-
-const completedAssignments = computed(() => {
-    return props.assignments.filter(assignment => assignment.complete);
 })
 
 </script>
