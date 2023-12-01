f92cf2b9d9b04c24e6d07c0abed510e32b606e0f
SarahH <sarah.heanan@komododigital.co.uk>
2023-12-01 15:55:29 +0000

    Add form
    Method to add assignment

--- resources/js/Components/Assignments.vue
+++ resources/js/Components/Assignments.vue
@@ -1,14 +1,24 @@
 <template>
     <AssignmentList :assignments="inProgressAssignments" title="In Progress"></AssignmentList>
     <AssignmentList :assignments="completedAssignments" title="Completed"></AssignmentList>
+
+    <form action="#" class="flex justify-between" @submit.prevent="add">
+        <input id="item" v-model="form.item" type="text" class="m-2 px-2 py-1 border border-gray-400" placeholder="Add item">
+        <button type="submit" class="ml-2 px-6 rounded-lg bg-gray-400">Add</button>
+    </form>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, reactive } from 'vue';
 import AssignmentList from '@/Components/AssignmentList.vue';
 
 const props = defineProps({
     assignments: Object,
+    newAssignment: String,
+})
+
+const form = reactive({
+    item: null,
 })
 
 const inProgressAssignments = computed(() => {
@@ -18,6 +28,16 @@ const inProgressAssignments = computed(() => {
 const completedAssignments = computed(() => {
     return props.assignments.filter(assignment => assignment.complete);
 })
+
+function add() {
+    props.assignments.push({
+        name: form.item,
+        completed: false,
+        id: props.assignments.length + 1
+    })
+
+    form.item = '';
+}
 
 </script>
 
