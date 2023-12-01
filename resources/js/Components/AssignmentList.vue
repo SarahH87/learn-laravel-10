7243b60ad2f021bafc49af9abeb7144beaacc626
SarahH <sarah.heanan@komododigital.co.uk>
2023-12-01 12:55:19 +0000

    Extract assignment list component
    In progress and completed separate instances now

---
+++ resources/js/Components/AssignmentList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section v-show="assignments.length">
+        <h2 class="text-xl font-bold mt-8 mb-2">{{ title }}</h2>
+
+        <ul>
+            <li v-for="assignment in assignments"
+                :key="assignment.id">
+                <label>
+                    {{ assignment.name }}
+
+                    <input type="checkbox" v-model="assignment.complete">
+                </label>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+
+defineProps({
+    assignments: Object,
+    title: String,
+})
+
+</script>
+
--- resources/js/Components/Assignments.vue
+++ resources/js/Components/Assignments.vue
@@ -1,37 +1,11 @@
 <template>
-    <section v-show="inProgressAssignments.length">
-        <h2 class="text-xl font-bold mt-8 mb-2">In Progress</h2>
-
-        <ul>
-            <li v-for="assignment in inProgressAssignments"
-                :key="assignment.id">
-                <label>
-                    {{ assignment.name }}
-
-                    <input type="checkbox" v-model="assignment.complete">
-                </label>
-            </li>
-        </ul>
-    </section>
-
-    <section v-show="completedAssignments.length" class="mt-8">
-        <h2 class="text-xl font-bold">Completed</h2>
-
-        <ul>
-            <li v-for="assignment in completedAssignments"
-                :key="assignment.id">
-                <label>
-                    {{ assignment.name }}
-
-                    <input type="checkbox" v-model="assignment.complete">
-                </label>
-            </li>
-        </ul>
-    </section>
+    <AssignmentList :assignments="inProgressAssignments" title="In Progress"></AssignmentList>
+    <AssignmentList :assignments="completedAssignments" title="Completed"></AssignmentList>
 </template>
 
 <script setup>
 import { computed } from 'vue'
+import AssignmentList from '@/Components/AssignmentList.vue';
 
 const props = defineProps({
     assignments: Object,
