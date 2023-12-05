586e9450c621243b9406c91f956113e1b3e2ac13
SarahH <sarah.heanan@komododigital.co.uk>
2023-12-05 14:23:30 +0000

    Main controller conditionals
    Get assignments from db

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assignment;
 use App\Models\User;
 use Inertia\Inertia;
 
@@ -19,65 +20,9 @@ class MainController extends Controller
 
     public function conditionals()
     {
-        $assignments = [
-            [
-                'name' => 'Finish shopping',
-                'complete' => false,
-                'id' => 1,
-                'tag' => 'home',
-            ],
-            [
-                'name' => 'Learn Vue',
-                'complete' => false,
-                'id' => 2,
-                'tag' => 'vue',
-            ]                ,
-            [
-                'name' => 'Advanced Laravel recap',
-                'complete' => false,
-                'id' => 3,
-                'tag' => 'laravel'
-            ],
-            [
-                'name' => 'Read Laravel documentation',
-                'complete' => false,
-                'id' => 4,
-                'tag' => 'laravel'
-            ],
-            [
-                'name' => 'Learn Docker',
-                'complete' => false,
-                'id' => 5,
-                'tag' => 'docker',
-            ],
-            [
-                'name' => 'Build API',
-                'complete' => true,
-                'id' => 6,
-                'tag' => 'laravel'
-            ],
-            [
-                'name' => 'Inertia Js course',
-                'complete' => false,
-                'id' => 7,
-                'tag' => 'laravel'
-            ],            [
-                'name' => 'Learn PEST',
-                'complete' => false,
-                'id' => 7,
-                'tag' => 'laravel'
-            ],
-            [
-                'name' => 'Read into AWS',
-                'complete' => false,
-                'id' => 8,
-                'tag' => 'laravel'
-            ],
-        ];
-
         return Inertia::render('Conditionals', [
             'currentPage' => 'home',
-            'assignments' => $assignments,
+            'assignments' => Assignment::all(),
         ]);
     }
 }
